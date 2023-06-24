47867f4b41b42987fafcf054fa366d33b67a8d7c
Yaroslav82 <yargay230607@gmail.com>
2023-06-24 13:08:33 +0300

    Convert field view into local variable

--- src/app/controllers/OrderAController.java
+++ src/app/controllers/OrderAController.java
@@ -8,14 +8,13 @@ import app.views.OrderView;
 
 public class OrderAController implements BaseController {
 
-    private OrderView view;
     private OrderAModel model;
     private String[] data;
     private String costRounded;
 
     @Override
     public void handleData() {
-        view = new OrderView();
+        OrderView view = new OrderView();
         data = view.getData();
         model = new OrderAModel();
         costRounded = Rounder.roundValue(getOrderCost());
